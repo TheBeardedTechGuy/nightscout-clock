b87328743bac1526a839beae57d4f40598c54b21
TheBeardedTechGuy <thebeardedtechdude@gmail.com>
2025-01-20 12:19:11 -0500

    Increase version to 0.14.1
    Increase version to 0.14.1

--- src/globals.h
+++ src/globals.h
@@ -3,7 +3,7 @@
 #include <Arduino.h>
 #include <IPAddress.h>
 
-#define VERSION "0.14"
+#define VERSION "0.14.1"
 
 #ifdef DEBUG
 #define DEBUG_PRINTLN(x)                                                                                                         \
