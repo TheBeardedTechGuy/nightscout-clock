618622feb744f1dccb72cc418d8fce6993539520
TheBeardedTechGuy <thebeardedtechdude@gmail.com>
2025-07-01 23:01:11 -0400

    Update script.js
    remove button enable functionality for no data timer

--- data/script.js
+++ data/script.js
@@ -41,7 +41,6 @@
         $("#btn_save").on('click', validateAndSave);
         $('#additional_wifi_enable').on('change', toggleAdditionalWifiSettings);
         $('#custom_hostname_enable').on('change', toggleCustomHostnameSettings);
-        $('#custom_nodatatimer_enable').on('change', toggleCustomNoDataSettings);
 
     }
 
@@ -80,11 +79,6 @@
         $('#custom_hostname_settings').toggleClass('d-none', !isChecked);
     }
     
-    function toggleCustomNoDataSettings() {
-        const isChecked = $('#custom_nodatatimer_enable').is(':checked');
-        $('#custom_nodatatimer_settings').toggleClass('d-none', !isChecked);
-    }
-
     function tryAlarm(e) {
         const alarmType = $(e.target).attr('id').replace('_alarm_try', '').replace('btn_', '');
 
@@ -482,10 +476,6 @@
         json['custom_hostname_enable'] = $('#custom_hostname_enable').is(':checked');
         json['custom_hostname'] = $('#custom_hostname').val();
 
-         // Custom No Data Timer
-        json['custom_nodatatimer_enable'] = $('#custom_nodatatimer_enable').is(':checked');
-        json['custom_nodatatimer'] = $('#custom_nodatatimer').val();
-
         return JSON.stringify(json);
     }
 
@@ -763,11 +753,6 @@
         $('#custom_hostname').val(json['custom_hostname']);
         toggleCustomHostnameSettings();
 
-         // Custom No Data Timer
-        $('#custom_nodatatimer_enable').prop('checked', json['custom_nodatatimer_enable']);
-        $('#custom_nodatatimer').val(json['custom_nodatatimer']);
-        toggleCustomNoDataSettings();
-        
     }
 
     function loadAlarmDataFromJson(json, alarmType) {
